d6ec026abb5ab24cfcbcfa625a228533c67c7680
Reusvillar <villarreus@gmail.com>
2025-01-17 13:14:07 +0100

    Esta un poco mejor el menu hamburguesa, seguire trabajando en ello

--- js/funciones.js
+++ js/funciones.js
@@ -1,6 +1,8 @@
-const miboton = document.getelementbyid("miboton");
-const minav = document.getelementbyid("miboton");
+// Selección de elementos
+const miboton = document.getElementById("miboton");
+const minav = document.getElementById("minav");
 
-miboton.addeventListener('click', () => {
-  minav.classList.toggle('show');
+// Evento para el botón hamburguesa
+miboton.addEventListener("click", () => {
+  minav.classList.toggle("show"); // Alterna la clase 'show'
 });
