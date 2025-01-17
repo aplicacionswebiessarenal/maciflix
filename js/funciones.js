82f8ff31c3495140c0d0d41fa86343f115c92b7e
Reusvillar <villarreus@gmail.com>
2025-01-17 13:35:17 +0100

    Arreglado header porfin

--- js/funciones.js
+++
@@ -1,8 +0,0 @@
-// Selección de elementos
-const miboton = document.getElementById("miboton");
-const minav = document.getElementById("minav");
-
-// Evento para el botón hamburguesa
-miboton.addEventListener("click", () => {
-  minav.classList.toggle("show"); // Alterna la clase 'show'
-});
