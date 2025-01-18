df16ca351c40b3ff47e80f5e49c4e2d6dd5555a3
gabitomaster3mil <gcampos@iessarenal.net>
2025-01-18 19:01:57 +0100

    he arreglado el botoncarrito.js para que funcione y arreglado tienda.html y carrito.html con localStorage

--- js/botoncarrito.js
+++ js/botoncarrito.js
@@ -7,12 +7,13 @@ botones.forEach(button => {
         const idproducto = button.getAttribute('data-id');
         const nombreproducto = button.getAttribute('data-name');
         const precioproducto = button.getAttribute('data-price');
-
+        const imagenproducto = button.getAttribute('data-image');
 
         const producto = {
             id: idproducto,
             name: nombreproducto,
             price: parseFloat(precioproducto),
+            image: imagenproducto,
         };
 
         cart.push(producto);
@@ -28,23 +29,28 @@ botones.forEach(button => {
 // Logica de carrito html
 if (window.location.pathname.includes('carrito.html')) {
     const carritoDiv = document.getElementById('carrito');
-
-
     const añadidocarrito = JSON.parse(localStorage.getItem('cart')) || [];
 
 
     if (añadidocarrito.length > 0) {
+        let total = 0;
         añadidocarrito.forEach(producto => {
             const productoDiv = document.createElement('div');
+            productoDiv.classList.add('producto-carrito')
             productoDiv.innerHTML =`
-                <h2>${producto.name}</h2>
-                <p>Precio: $${producto.price}</p>
+                <img src="${producto.image}" alt="${producto.name}" class="imagen-producto" />
+                <div class="detalle-producto">
+                    <h3>${producto.name}</h3>
+                    <p>Precio: $${producto.price.toFixed(2)}</p>
+                </div>
         `;
         carritoDiv.appendChild(productoDiv);
 
+        total += producto.price;
     });
-
-    } else {
+    
+document.getElementById('total').textContent = total.toFixed(2);
+} else {
         carritoDiv.innerHTML = '<p>Tu carrito está vacío.</p>';
     }
 }
