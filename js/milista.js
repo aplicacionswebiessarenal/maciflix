0b4c9a9b431c7e8ec10fc6bcb567ca5b5daf2b18
sfallous <syllaserignefallou907@gmail.com>
2025-01-20 12:10:25 +0100

    he areglado el faq

--- js/milista.js
+++ js/milista.js
@@ -11,17 +11,3 @@ function hidePopup() {
 
 
 /*script para el faq.html*/
-const faqPreguntas = document.querySelectorAll('.faq-pregunta');
-
-faqPreguntas.forEach((pregunta) => {
-    pregunta.addEventListener('click', () => {
-        const respuesta = pregunta.nextElementSibling;
-
-        if (respuesta.style.display === 'block') {
-            respuesta.style.display = 'none';
-        } else {
-            respuesta.style.display = 'block';
-        }
-    });
-});
-
