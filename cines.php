369544bd97e9ce222a2d72964069f392b49e2857
XavierJaner1 <xjanerc@iessarenal.net>
2025-02-14 14:00:08 +0100

    los cines estan a php

--- cines.php
+++ cines.php
@@ -14,61 +14,46 @@
     <body>
     <!-- Titulo -->
     <iframe src="header.php" onload="this.before((this.contentDocument.body||this.contentDocument).children[0]);this.remove()"></iframe>
-        <h1>Cines disponibles</h1> 
+        <h1>Cines disponibles</h1>
     <!-- Las peliculas disponibles -->
         <div class="contenedor-de-cines">
-            <div class="foto_y_nombre">
-                <div class="cine-individual" id="cine1">
-                    <div class="fotocine">
-                        <a href="/peliculasdisponiblescines.php">
-                            <img src="/img/cine1.png" alt="Cine Tampa">
-                        </a>
+            <?php
+                $sql = "SELECT id, name, img, address, email, timetable, telephone, instagram, twitter, facebook FROM cinemas";
+                $result = $bbdd->query($sql);
+                while ($row = $result->fetch_assoc()): ?>
+                    <div class="foto_y_nombre">
+                        <div class="cine-individual">
+                            <div class="fotocine">
+                                <a href="peliculasdisponiblescines.php?id=<?= $row['id'] ?>">
+                                    <img src="<?= htmlspecialchars($row['img']) ?>" alt="<?= htmlspecialchars($row['name']) ?>">
+                                </a>
+                            </div>
+                            <div class="cine_titulo">
+                                <div class="cine_titulo_1"><?= htmlspecialchars($row['name']) ?></div>
+                            </div>
+                        </div>
+                        <div class="informacion_del_cine">
+                            <div class="informacion-general">
+                                <span><a href="https://maps.app.goo.gl/"><?= htmlspecialchars($row['address']) ?></a></span><br>
+                                <span>Teléfono de contacto: <a href="tel:<?= htmlspecialchars($row['telephone']) ?>"><?= htmlspecialchars($row['telephone']) ?></a></span><br>
+                                <span>Correo electrónico: <address><a href="mailto:<?= htmlspecialchars($row['email']) ?>"><?= htmlspecialchars($row['email']) ?></a></address><br>
+                                <span>Horarios de atención: <?= nl2br(htmlspecialchars($row['timetable'])) ?></span><br>
+                                <span>Redes Sociales:</span>
+                                <ul class="redes-sociales">
+                                    <?php if ($row['facebook']): ?>
+                                        <li><span><a href="<?= htmlspecialchars($row['facebook']) ?>">Facebook</a></span></li>
+                                    <?php endif; ?>
+                                    <?php if ($row['instagram']): ?>
+                                        <li><span><a href="<?= htmlspecialchars($row['instagram']) ?>">Instagram</a></span></li>
+                                    <?php endif; ?>
+                                    <?php if ($row['twitter']): ?>
+                                        <li><span><a href="<?= htmlspecialchars($row['twitter']) ?>">Twitter</a></span></li>
+                                    <?php endif; ?>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
-                    <div class="cine_titulo">
-                        <div class="cine_titulo_1">Cine Tampa</div>
-                    </div>
-                </div>
-                <div class="informacion_del_cine">
-                    <div class="informacion-general">
-                        <span><a href="https://maps.app.goo.gl/wFJKjSYA9vf4Er7E7">Direccion: Carrer de l'Olivar, 07001 Palma, Mallorca</a></span><br>
-                        <span>Teléfono de contacto: <a href="tel:+6494461709">(+34) 123 456 789</a></span><br>
-                        <span>Correo electrónico: <address><a href="mailto:contacto@cinemallorca.com">contacto@cinemallorca.com</a></address><br>
-                        <span>Horarios de atención: Lunes a Domingo: 10:00 - 22:00</span><br>
-                        <span>Redes Sociales:</span>
-                        <ul class="redes-sociales"> 
-                            <li><span><a href="https://es-es.facebook.com/">Facebook</a></span></a></li>
-                            <li><span><a href="https://es-es.facebook.com/">Instagram</a></span></a></li>
-                            <li><span><a href="https://es-es.facebook.com/">Twitter</a></span></a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="foto_y_nombre">
-                <div class="cine-individual" id="cine1">
-                    <div class="fotocine">
-                        <a href="/peliculasdisponiblescines.php">
-                            <img src="/img/cine2.png" alt="Cine Cosmo">
-                        </a>
-                    </div>
-                    <div class="cine_titulo">
-                        <div class="cine_titulo_1">Cine Cosmo</div>
-                    </div>
-                </div>
-                <div class="informacion_del_cine">
-                    <div class="informacion-general">
-                        <span><a href="https://maps.app.goo.gl/d91zTn6suQiD1rx56">Direccion: Carrer del Monestir, 5, 07620 Llucmajor, Mallorca</a></span><br>
-                        <span>Teléfono de contacto: <a href="tel:+6494461709">(+34) 636 35 52 89</a></span><br>
-                        <span>Correo electrónico: <address><a href="mailto:contacto@cinemallorca.com">contacto@cinemallorca.com</a></address><br>
-                        <span>Horarios de atención: Lunes a Domingo: 10:00 - 22:00</span><br>
-                        <span>Redes Sociales:</span>
-                        <ul>
-                            <li><span><a href="https://es-es.facebook.com/">Facebook</a></span></a></li>
-                            <li><span><a href="https://es-es.facebook.com/">Instagram</a></span></a></li>
-                            <li><span><a href="https://es-es.facebook.com/">Twitter</a></span></a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
+            <?php endwhile; ?>
         </div>
         <script src="/js/cines.js"></script>
         <iframe src="footer.php" onload="this.before((this.contentDocument.body||this.contentDocument).children[0]);this.remove()"></iframe>
