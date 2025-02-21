5bfb96a284cfb571f2927a403c9dd8896b267cc8
Torreblanca <sashareconquista@proton.me>
2025-02-21 12:33:07 +0100

    SE PUEDEN CREAR USUARIOS YUPIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII

--- registrarse.php
+++ registrarse.php
@@ -27,27 +27,27 @@
     <div class="login">
       <form action="registrarse.php" method="POST">
         <h2 id="title">Crea tu cuenta</h2>
-        <form action="process.php" method="POST">
-          <input type="text" name="username" class="password" placeholder="Nombre de usuario" required />
 
-          <input type="email" name="email" id="email" placeholder="Email" required />
-          <input type="password" name="password" class="password" placeholder="Contraseña" required />
-          <input type="password" name="confirm_password" class="password" placeholder="Confirma Contraseña" required />
-          <input type="text" name="payment_method" class="password" placeholder="Numero de tarjeta" />
-          <input type="text" name="address" class="password" placeholder="Dirección" />
-          <input type="text" name="first_name" class="password" placeholder="Nombre" required />
-          <input type="text" name="last_name1" class="password" placeholder="Apellido 1" required />
-          <input type="text" name="last_name2" class="password" placeholder="Apellido 2" required />
-          <button type="submit">Enviar</button>
-        </form>
-        <div class="register">
-          <h3>Ya tienes una cuenta?</h3>
-          <a href="/iniciarsesion.php">
-            <button type="button" id="register" class="log_in_button glow_on_hover">
-              Iniciar sesión
-            </button>
-          </a>
-        </div>
+        <input type="text" name="username" class="password" placeholder="Nombre de usuario" required />
+
+        <input type="email" name="email" id="email" placeholder="Email" required />
+        <input type="password" name="password" class="password" placeholder="Contraseña" required />
+        <input type="password" name="confirm_password" class="password" placeholder="Confirma Contraseña" required />
+        <input type="text" name="payment_method" class="password" placeholder="Numero de tarjeta" />
+        <input type="text" name="address" class="password" placeholder="Dirección" />
+        <input type="text" name="first_name" class="password" placeholder="Nombre" required />
+        <input type="text" name="last_name1" class="password" placeholder="Apellido 1" required />
+        <input type="text" name="last_name2" class="password" placeholder="Apellido 2" required />
+        <button type="submit">Enviar</button>
+      </form>
+      <div class="register">
+        <h3>Ya tienes una cuenta?</h3>
+        <a href="/iniciarsesion.php">
+          <button type="button" id="register" class="log_in_button glow_on_hover">
+            Iniciar sesión
+          </button>
+        </a>
+      </div>
       </form>
     </div>
   </div>
@@ -82,8 +82,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $password_hash = password_hash($password, PASSWORD_DEFAULT);
 
     $stmt = $conn->prepare("INSERT INTO users (username, email, password, address, payment_method,name, surname1, surname2) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
-    $stmt->bind_param("ssssssss", $username, $email, $hashed_password, $address, $card_number, $first_name, $last_name1, $last_name2);
-    $stmt->close();
+    if ($stmt) {
+      $stmt->bind_param("ssssssss", $username, $email, $password_hash, $address, $card_number, $first_name, $last_name1, $last_name2);
+      if ($stmt->execute()) {
+        header("Location: iniciarsesion.php?registration=success");
+        exit();
+      } else {
+        echo "Error: " . $stmt->error;
+      }
+      $stmt->close();
+    } else {
+      echo "Error preparing statement: " . $conn->error;
+    }
+
   }
 }
 ?>
