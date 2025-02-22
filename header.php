4f345f69c035383f77ce184b1c3a1626dd31aa9f
Jinx <sashareconquista@proton.me>
2025-02-22 15:40:41 +0100

    Trabajando en las galletitas

--- header.php
+++ header.php
@@ -1,31 +1,34 @@
-<?php include_once('conexion.php'); ?> 
+<?php include_once('conexion.php'); ?>
 <!DOCTYPE html>
+
 <head>
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
   <link
     href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
-    rel="stylesheet"
-  />
+    rel="stylesheet" />
 </head>
 <header>
 
 
   <div class="header">
-      <div>
-        <a href="index.php">
-          <img class="icono" src="img/logomaciflix.png" alt="icono"/>
-        </a>
-        <!--Icono Maciflix-->
-      </div>
-      <div>
+    <div>
+      <a href="index.php">
+        <img class="icono" src="img/logomaciflix.png" alt="icono" />
+      </a>
+      <!--Icono Maciflix-->
+    </div>
+    <div>
+      <?php if (!isset($_SESSION['user_id'])): ?>
         <a href="iniciarsesion.php">
           <button id="iniciosesión" class="minimalbutton">
             Iniciar Sesión
           </button>
         </a>
-      </div>
-    </div> 
+      <?php endif; ?>
+
+    </div>
+  </div>
   </div>
   <nav>
     <button class="miboton" id="miboton" onclick="minav.classList.toggle('show');">
